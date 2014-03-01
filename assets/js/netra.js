c8361a74b28a85e64a64506ae9f6a0ae7e1e8a61
Arie <arie@wego.com>
2014-03-02 01:31:26 +0700

    update and optimize content

--- assets/js/netra.js
+++ assets/js/netra.js
@@ -1,7 +1,6 @@
 $(function() {
 	  $('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
 	      var target = $(this.hash);
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 	      if (target.length) {
@@ -13,3 +12,12 @@ $(function() {
 	    }
 	  });
 	});
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-inverse").css("background-color", "#1a1a1c");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-inverse").css("background-color", "transparent");
+    }
+});
